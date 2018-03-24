74f2859ac7f8203f3c0b751205f35e00056b1f02
Matthew Chrobak <matthewchrobak@hotmail.com>
2018-03-23 23:01:13 -0400

    Camera stuff

--- Source/Game/Graphics/GraphicsSystem.cs
+++ Source/Game/Graphics/GraphicsSystem.cs
@@ -6,19 +6,28 @@ namespace Game.Graphics
     {
         private GameWindow _context;
         private SurfaceManager _surfaces;
+        private Camera _camera;
 
         public GraphicsSystem()
         {
+            this._camera = new Camera();
             this._context = new GameWindow();
             this._surfaces = new SurfaceManager();
+
+            this._context.SetView(this._camera);
 
             this._surfaces.LoadTextures();
         }
 
         public void Render()
         {
-            this._context.Clear(Color.Red);
+            this._context.Clear(Color.Black);
             this._context.DispatchEvents();
+
+            var grass = this._surfaces.GetTextureByName("grass.png");
+            grass.Scale = new SFML.System.Vector2f(5000, 5000);
+            this._context.Draw(grass);
+
             this._context.Display();
         }
     }
