42ff10356643f83ae764f16a8f2fa269083897d1
Darkheros12 <g.poseidon@hotmail.com>
2018-03-23 23:04:49 -0400

    LittleGirl, Item, Inventory
    Abstract funtionality for littlegirl.
    direction,
    inventory,
    health

---
+++ Source/Game/Models/LittleGirl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Game.Models
+{
+    public class LittleGirl
+    {
+        enum direction
+        {
+            left, right, up, down
+        };
+
+        public int health { get; set; }
+        public Inventory littleGirlInventory;
+    }
+}
+
+
